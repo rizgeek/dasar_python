0945fcee93f83d4778fee98dfa5556de941b2e7a
Muhammad Rizki <rizki@madeindonesia.com>
2021-10-01 19:54:39 +0700

    casting_tipe_data

---
+++ casting_tipe_data.py
@@ -0,0 +1,14 @@
+# Casting adalah merubah tipe data
+# tipe data = int, float, str, boolean 
+
+data_int = 3
+print("data = ", data_int, type(data_int))
+
+data_float = float(data_int)
+data_str = str(data_int)
+data_bool = bool(data_int) # akan false jika 0 (nol) atau string kosong '' 
+
+
+print("data = ", data_float, type(data_float))
+print("data = ", data_str, type(data_str))
+print("data = ", data_bool, type(data_bool)) 
